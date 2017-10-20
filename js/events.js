064a708a67f4c31718720ae71fdb27ce8a98599f
Shumei Huang <rose1264@gmail.com>
2017-10-20 13:50:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,10 +19,17 @@ function pressIt(){
   })
 }
 
+function submitIt(){
+  $('form').on('sumbit',function(){
+    alert('Your form is going to be submitted now.'
+    )
+  })
+}
 $(document).ready(function(){
 
 // call functions here
 getIt()
 frameIt()
-
+pressIt()
+submitIt()
 });
