a242237e328bf8b6952fa5092ad684e0123685d0
Shumei Huang <rose1264@gmail.com>
2017-10-20 13:55:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,11 +20,11 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form').on('sumbit',function(){
-    alert('Your form is going to be submitted now.'
-    )
+  $('form').on('sumbit',function(e){
+    alert('Your form is going to be submitted now.')
   })
 }
+
 $(document).ready(function(){
 
 // call functions here
